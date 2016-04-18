71ecfd8ab75b07bbe32f7db25a86956279e831d7
Rudnef <e.rudnef@gmail.com>
2016-04-19 02:43:50 +0300

    добавлены передают сообщения в связке "ключ-значение"

--- P0501_Twoscreens/app/src/main/java/com/example/rudnef/p0501_twoscreens/MainActivity.java
+++ P0501_Twoscreens/app/src/main/java/com/example/rudnef/p0501_twoscreens/MainActivity.java
@@ -5,9 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,27 @@ public class MainActivity extends AppCompatActivity {
 
     public void onSecond(View view) {
         Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    public void onSend(View view) {
+        EditText et_name = (EditText) findViewById(R.id.et_name);
+        EditText et_message = (EditText) findViewById(R.id.et_message);
+        EditText et_from_who = (EditText) findViewById(R.id.et_from_who);
+
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+
+        // если вьюха не равна нулю присваиваем интенту с ключем, значение
+        // далее стартуем интент
+        if (et_name != null ) {
+            intent.putExtra("username", et_name.getText().toString());
+        }
+
+        assert et_message != null;
+        intent.putExtra("gift", et_message.getText().toString());
+
+        assert et_from_who != null;
+        intent.putExtra("from_who", et_from_who.getText().toString());
         startActivity(intent);
     }
 }
