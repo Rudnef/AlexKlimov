8b6502ed9530b39099e0b3e34300cf694c015685
Rudnef <e.rudnef@gmail.com>
2016-04-18 14:08:52 +0300

    добавлена кликабельна картинка, при клике на которую появляется приветствие

--- P0201_hello_Kitty/app/src/main/java/com/example/rudnef/p0201_hello_kitty/MainActivity.java
+++ P0201_hello_Kitty/app/src/main/java/com/example/rudnef/p0201_hello_kitty/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.rudnef.p0201_hello_kitty;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv_showText;
+    ImageButton ib_kitty;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ib_kitty = (ImageButton) findViewById(R.id.ib_kitty);
+        tv_showText = (TextView) findViewById(R.id.tv_showText);
+
+        ib_kitty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv_showText.setText("Hello Kitty");
+            }
+        });
     }
 }
